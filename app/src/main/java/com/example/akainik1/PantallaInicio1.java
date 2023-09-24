08f644fa2e6b6b27f8e3ddea5d2197dfdfa851f7
FelipePalta <f.palta2@alumnos.santotomas.cl>
2023-09-24 20:51:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/akainik1/PantallaInicio1.java
@@ -0,0 +1,14 @@
+package com.example.akainik1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInicio1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio1);
+    }
+}
